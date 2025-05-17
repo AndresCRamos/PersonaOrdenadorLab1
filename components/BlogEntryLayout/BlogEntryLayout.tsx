fcf98a7fdd87d6105c076877a598cf411138a240
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-05-17 00:36:41 -0500

    Add affix in Blog Entry to return to main page

--- components/BlogEntryLayout/BlogEntryLayout.tsx
+++ components/BlogEntryLayout/BlogEntryLayout.tsx
@@ -1,4 +1,5 @@
-import { Container, Stack, Title } from "@mantine/core";
+import { Affix, Button, Container, Space, Stack, Title } from "@mantine/core";
+import Link from "next/link";
 import React from "react";
 
 interface BlogEntryLayoutProps {
@@ -11,12 +12,21 @@ const BlogEntryLayout: React.FC<BlogEntryLayoutProps> = ({
   title,
 }) => {
   return (
-    <Container>
-      <Stack>
-        <Title ta={"center"}>{title}</Title>
-        {children}
-      </Stack>
-    </Container>
+    <>
+      <Container>
+        <Stack gap={"lg"}>
+          <Title ta={"center"}>{title}</Title>
+          {children}
+        </Stack>
+      </Container>
+      <Space h={"xl"} />
+      <Space h={"lg"} />
+      <Affix position={{ bottom: 20, right: 20 }}>
+        <Button component={Link} href={`/`}>
+          Volver
+        </Button>
+      </Affix>
+    </>
   );
 };
 
