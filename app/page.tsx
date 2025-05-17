3b00a3b593c1740a26ea8d13251f1e6d1cd63cbb
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-05-16 23:56:16 -0500

    Format Main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-import { Card, Center, Container, SimpleGrid, Stack, Text, Title } from '@mantine/core';
-import Link from 'next/link';
+import {
+  Card,
+  Center,
+  Container,
+  SimpleGrid,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import Link from "next/link";
 
 const blogEntries = [
-  { slug: 'web-dev', title: 'Desarrollo web', description: 'Introducción a desarrollo de interfaces y lógica web modernas.' },
-  { slug: 'html', title: 'HTML 5 semantico', description: 'Estructura semántica y accesibilidad con HTML5.' },
-  { slug: 'css', title: 'Responsive CSS', description: 'Diseño responsivo y layout con Flexbox y Grid.' },
-  { slug: 'js', title: 'JS y Next.js', description: 'Sitios interactiva con JavaScript y Next.js.' },
+  {
+    slug: "web-dev",
+    title: "Desarrollo web",
+    description:
+      "Introducción a desarrollo de interfaces y lógica web modernas.",
+  },
+  {
+    slug: "html",
+    title: "HTML 5 semantico",
+    description: "Estructura semántica y accesibilidad con HTML5.",
+  },
+  {
+    slug: "css",
+    title: "Responsive CSS",
+    description: "Diseño responsivo y layout con Flexbox y Grid.",
+  },
+  {
+    slug: "js",
+    title: "JS y Next.js",
+    description: "Sitios interactiva con JavaScript y Next.js.",
+  },
 ];
 
 export default function Page() {
@@ -20,18 +45,26 @@ export default function Page() {
 
       {/* Descripción breve */}
       <Text size="md" mb="xl">
-        El desarrollo de aplicaciones en red abarca la creación de software que se
-        autentica, comunica y procesa datos a través de redes. Incluye tanto el
-        front-end como el back-end, protocolos de comunicación y prácticas de seguridad
-        para garantizar aplicaciones robustas y escalables.
+        El desarrollo de aplicaciones en red abarca la creación de software que
+        se autentica, comunica y procesa datos a través de redes. Incluye tanto
+        el front-end como el back-end, protocolos de comunicación y prácticas de
+        seguridad para garantizar aplicaciones robustas y escalables.
       </Text>
       {/* Grid de tarjetas de entradas */}
-      <SimpleGrid cols={2} spacing="lg">  
+      <SimpleGrid cols={2} spacing="lg">
         {blogEntries.map(({ slug, title, description }) => (
-          <Card key={slug} shadow="sm" padding="lg" component={Link} href={`post/${slug}`}>  
+          <Card
+            key={slug}
+            shadow="sm"
+            padding="lg"
+            component={Link}
+            href={`post/${slug}`}
+          >
             <Stack gap="xs">
-              <Title order={4} ta={'center'}>{title}</Title>
-              <Text size="sm" c='dimmed' ta={'center'}>
+              <Title order={4} ta={"center"}>
+                {title}
+              </Title>
+              <Text size="sm" c="dimmed" ta={"center"}>
                 {description}
               </Text>
             </Stack>
