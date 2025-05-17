41827c281fbb511596aab10f75fb63b864079750
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-05-17 00:26:05 -0500

    Simplify BlogEntry center title

--- components/BlogEntryLayout/BlogEntryLayout.tsx
+++ components/BlogEntryLayout/BlogEntryLayout.tsx
@@ -1,25 +1,23 @@
-import { Center, Container, Stack, Title } from "@mantine/core";
+import { Container, Stack, Title } from "@mantine/core";
 import React from "react";
 
-
 interface BlogEntryLayoutProps {
-    children: React.ReactNode;
-    title: string;
+  children: React.ReactNode;
+  title: string;
 }
 
- 
-const BlogEntryLayout: React.FC<BlogEntryLayoutProps> = ({children, title}) => {
-    return <Container>
-          <Stack>
-            <Center>
-                <Title>{title}</Title>
-            </Center>
-            {children}
-          </Stack>
-        </Container>
-}
+const BlogEntryLayout: React.FC<BlogEntryLayoutProps> = ({
+  children,
+  title,
+}) => {
+  return (
+    <Container>
+      <Stack>
+        <Title ta={"center"}>{title}</Title>
+        {children}
+      </Stack>
+    </Container>
+  );
+};
 
-
-
- 
 export default BlogEntryLayout;
